595f91315b0d5302b768c8e616abae4702826601
Daniel Berger <dberger@redhat.com>
2015-05-14 13:17:35 -0600

    Added ArmRest scope/module.

--- lib/azure/armrest/virtual_machine_manager.rb
+++ lib/azure/armrest/virtual_machine_manager.rb
@@ -1,61 +1,63 @@
 module Azure
-  class VirtualMachineManager
-    COMMON_URI = "https://management.azure.com/subscriptions"
+  module ArmRest
+    class VirtualMachineManager
+      COMMON_URI = "https://management.azure.com/subscriptions"
 
-    attr_accessor :subscription_id
-    attr_accessor :resource_group_name
-    attr_accessor :uri
-    attr_accessor :api_version
+      attr_accessor :subscription_id
+      attr_accessor :resource_group_name
+      attr_accessor :uri
+      attr_accessor :api_version
 
-    def initialize(name, subscription_id, resource_group_name, api_version = '2014-02-14-preview')
-      @subscription_id = subscription_id
-      @resource_group_name = resource_group_name
-      @api_version = api_version
+      def initialize(name, subscription_id, resource_group_name, api_version = '2014-02-14-preview')
+        @subscription_id = subscription_id
+        @resource_group_name = resource_group_name
+        @api_version = api_version
 
-      @uri = COMMON_URI + "/#{subscription_id}"
-      @uri += "/resourceGroups/#{resource_group_name}"
-      @uri += "/providers/Microsoft.Compute/VirtualMachines"
-    end
+        @uri = COMMON_URI + "/#{subscription_id}"
+        @uri += "/resourceGroups/#{resource_group_name}"
+        @uri += "/providers/Microsoft.Compute/VirtualMachines"
+      end
 
-    # POST
-    def capture
-    end
+      # POST
+      def capture
+      end
 
-    # PUT
-    def create(options = {})
-    end
+      # PUT
+      def create(options = {})
+      end
 
-    alias update create
+      alias update create
 
-    def deallocate
-    end
+      def deallocate
+      end
 
-    # DELETE
-    def delete
-    end
+      # DELETE
+      def delete
+      end
 
-    # POST
-    def generalize
-    end
+      # POST
+      def generalize
+      end
 
-    # GET
-    def get(model_view = true)
-    end
+      # GET
+      def get(model_view = true)
+      end
 
-    # GET
-    def operations
-    end
+      # GET
+      def operations
+      end
 
-    # POST
-    def restart
-    end
+      # POST
+      def restart
+      end
 
-    # POST
-    def start
-    end
+      # POST
+      def start
+      end
 
-    # POST
-    def stop
+      # POST
+      def stop
+      end
     end
   end
 end
