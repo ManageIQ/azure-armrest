71fb0e79f1ccc6a8b84f1cd047e132eed45faf23
Daniel Berger <dberger@redhat.com>
2015-05-27 09:06:26 -0600

    Modified to be a subclass of VirtualMachineManager, and methods updated so they don't modify @uri.

--- lib/azure/armrest/virtual_machine_extension_manager.rb
+++ lib/azure/armrest/virtual_machine_extension_manager.rb
@@ -3,18 +3,7 @@ module Azure
   # ArmRest namespace
   module ArmRest
     # Base class for managing virtual machine extensions
-    class VirtualMachineExtensionManager < ArmRestManager
-
-      # Create and return a new VirtualMachineExtensionManager (VMEM) instance.
-      # Most methods for a VME instance will return one or more VirtualMachine
-      # instances.
-      #
-      def initialize(subscription_id, resource_group_name, api_version = '2015-1-1')
-        super
-
-        @uri += "/resourceGroups/#{resource_group_name}"
-        @uri += "/providers/Microsoft.Compute/virtualMachines"
-      end
+    class VirtualMachineExtensionManager < VirtualMachineManager
 
       # Creates a new virtual machine extension for +vmname+ with the given
       # +extension_name+, and the given +options+. Possible options are:
@@ -31,7 +20,8 @@ module Azure
         type = options.fetch(:type)
         type_handler_version = optios.fetch(:type_handler_version)
 
-        @uri += "/#{vmname}/extensions/#{extension_name}?#{api_version}"
+        url = @uri + "/#{vmname}/extensions/#{extension_name}?#{api_version}"
+        url
       end
 
       # Delete the given +extension_name+ for +vmname+.
@@ -39,7 +29,8 @@ module Azure
       # DELETE
       #
       def delete(vmname, extension_name)
-        @uri += "/#{vmname}/extensions/#{extension_name}?#{api_version}"
+        url = @uri + "/#{vmname}/extensions/#{extension_name}?#{api_version}"
+        url
       end
 
       # Retrieves the settings of an extension. If the +instance_view+ option
@@ -48,16 +39,18 @@ module Azure
       # GET
       #
       def get(vmname, instance_view = false)
-        @uri += "/#{vmname}/extensions/#{extension_name}?"
-        @uri += "$expand=instanceView," if instance_view
-        @uri += "#{api_version}"
+        url = @uri + "/#{vmname}/extensions/#{extension_name}?"
+        url += "$expand=instanceView," if instance_view
+        url += "#{api_version}"
+        url
       end
 
       # Retrieves a list of extensions on the VM.
       def list(vmname, instance_view = false)
-        @uri += "/#{vmname}/extensions"
-        @uri += "$expand=instanceView," if instance_view
-        @uri += "#{api_version}"
+        url = @uri + "/#{vmname}/extensions"
+        url += "$expand=instanceView," if instance_view
+        url += "#{api_version}"
+        url
       end
     end
   end
