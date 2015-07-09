d04907cd498728c17e82ea3fe6a2c70928cc6864
Daniel Berger <dberger@redhat.com>
2015-07-09 11:15:47 -0400

    Added some basic specs for existing and new instance methods.

--- spec/armrest_manager_spec.rb
+++ spec/armrest_manager_spec.rb
@@ -3,7 +3,6 @@
 #
 # Test suite for the Azure::Armrest::ArmrestManager class.
 ########################################################################
-
 require 'spec_helper'
 
 describe "ArmrestManager" do
@@ -13,6 +12,49 @@ describe "ArmrestManager" do
   context "constructor" do
     it "returns an armrest manager instance as expected" do
       expect(arm).to be_kind_of(Azure::Armrest::ArmrestManager)
+    end
+  end
+
+  context "methods" do
+    it "defines a providers method" do
+      expect(arm).to respond_to(:providers)
+    end
+
+    it "defines a provider_info method" do
+      expect(arm).to respond_to(:provider_info)
+    end
+
+    it "defines a geo_locations alias for provider_info" do
+      expect(arm).to respond_to(:geo_locations)
+      expect(arm.method(:geo_locations)
+    end
+
+    it "defines a resources method" do
+      expect(arm).to respond_to(:resources)
+    end
+
+    it "defines a resource_groups method" do
+      expect(arm).to respond_to(:resource_groups)
+    end
+
+    it "defines a resource_group_info method" do
+      expect(arm).to respond_to(:resource_group_info)
+    end
+
+    it "defines a subscriptions method" do
+      expect(arm).to respond_to(:subscriptions)
+    end
+
+    it "defines a subscription_info method" do
+      expect(arm).to respond_to(:subscription_info)
+    end
+
+    it "defines a tags method" do
+      expect(arm).to respond_to(:tags)
+    end
+
+    it "defines a tenants method" do
+      expect(arm).to respond_to(:tenants)
     end
   end
 
