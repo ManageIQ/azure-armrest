f805c4f492282369819ec4ed258b9c0fd4d38c75
Daniel Berger <dberger@redhat.com>
2015-07-17 12:49:59 -0500

    Updated to use rspec 3.

--- spec/availability_set_manager_spec.rb
+++ spec/availability_set_manager_spec.rb
@@ -3,7 +3,6 @@
 #
 # Test suite for the Azure::Armrest::AvailabilitySetManager class.
 ########################################################################
-
 require 'spec_helper'
 
 describe "AvailabilitySetManager" do
@@ -12,49 +11,49 @@ describe "AvailabilitySetManager" do
 
   context "inheritance" do
     it "is a subclass of ArmrestManager" do
-      Azure::Armrest::AvailabilitySetManager.ancestors.should include(Azure::Armrest::ArmrestManager)
+      expect(Azure::Armrest::AvailabilitySetManager.ancestors).to include(Azure::Armrest::ArmrestManager)
     end
   end
 
   context "constructor" do
     it "returns an asm instance as expected" do
-      asm.should be_kind_of(Azure::Armrest::AvailabilitySetManager)
+      expect(asm).to be_kind_of(Azure::Armrest::AvailabilitySetManager)
     end
 
     it "sets the default uri to the expected value" do
       expected = "https://management.azure.com"
       expected << "/resourceGroups/#{@res}/providers/Microsoft.Compute/availabilitySets"
-      asm.base_url.should eq(expected)
+      expect(asm.base_url).to eq(expected)
     end
   end
 
   context "accessors" do
     it "defines a base_url accessor" do
-      asm.should respond_to(:base_url)
-      asm.should respond_to(:base_url=)
+      expect(asm).to respond_to(:base_url)
+      expect(asm).to respond_to(:base_url=)
     end
   end
 
   context "instance methods" do
     it "defines a create method" do
-      asm.should respond_to(:create)
+      expect(asm).to respond_to(:create)
     end
 
     it "defines an update alias" do
-      asm.should respond_to(:update)
-      asm.method(:update).should eql(asm.method(:create))
+      expect(asm).to respond_to(:update)
+      expect(asm.method(:update)).to eql(asm.method(:create))
     end
 
     it "defines a delete method" do
-      asm.should respond_to(:delete)
+      expect(asm).to respond_to(:delete)
     end
 
     it "defines a get method" do
-      asm.should respond_to(:get)
+      expect(asm).to respond_to(:get)
     end
 
     it "defines a stop method" do
-      asm.should respond_to(:list)
+      expect(asm).to respond_to(:list)
     end
   end
 end
--- spec/virtual_machine_extension_manager_spec.rb
+++ spec/virtual_machine_extension_manager_spec.rb
@@ -13,43 +13,43 @@ describe "VirtualMachineExtensionManager" do
   context "inheritance" do
     it "is a subclass of VirtualMachineManager" do
       ancestors = Azure::Armrest::VirtualMachineExtensionManager.ancestors
-      ancestors.should include(Azure::Armrest::VirtualMachineManager)
+      expect(ancestors).to include(Azure::Armrest::VirtualMachineManager)
     end
   end
 
   context "constructor" do
     it "returns a vmem instance as expected" do
-      vmem.should be_kind_of(Azure::Armrest::VirtualMachineExtensionManager)
+      expect(vmem).to be_kind_of(Azure::Armrest::VirtualMachineExtensionManager)
     end
   end
 
   context "accessors" do
     it "defines a base_url accessor" do
-      vmem.should respond_to(:base_url)
-      vmem.should respond_to(:base_url=)
+      expect(vmem).to respond_to(:base_url)
+      expect(vmem).to respond_to(:base_url=)
     end
   end
 
   context "instance methods" do
     it "defines a create method" do
-      vmem.should respond_to(:create)
+      expect(vmem).to respond_to(:create)
     end
 
     it "defines an update alias" do
-      vmem.should respond_to(:update)
-      vmem.method(:update).should eql(vmem.method(:create))
+      expect(vmem).to respond_to(:update)
+      expect(vmem.method(:update)).to eql(vmem.method(:create))
     end
 
     it "defines a delete method" do
-      vmem.should respond_to(:delete)
+      expect(vmem).to respond_to(:delete)
     end
 
     it "defines a get method" do
-      vmem.should respond_to(:get)
+      expect(vmem).to respond_to(:get)
     end
 
     it "defines a list method" do
-      vmem.should respond_to(:list)
+      expect(vmem).to respond_to(:list)
     end
   end
 end
