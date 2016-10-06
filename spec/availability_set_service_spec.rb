bb2aa466e2cbf13ce8f73d4fac569e7e95258baf
Daniel Berger <dberger@redhat.com>
2016-10-06 09:57:54 -0600

    Improve variable name.

--- spec/availability_set_service_spec.rb
+++ spec/availability_set_service_spec.rb
@@ -7,7 +7,7 @@ require 'spec_helper'
 
 describe "AvailabilitySetService" do
   before { setup_params }
-  let(:ass) { Azure::Armrest::AvailabilitySetService.new(@conf) }
+  let(:avss) { Azure::Armrest::AvailabilitySetService.new(@conf) }
 
   context "inheritance" do
     it "is a subclass of ArmrestService" do
@@ -17,30 +17,30 @@ describe "AvailabilitySetService" do
 
   context "constructor" do
     it "returns an ASS instance as expected" do
-      expect(ass).to be_kind_of(Azure::Armrest::AvailabilitySetService)
+      expect(avss).to be_kind_of(Azure::Armrest::AvailabilitySetService)
     end
   end
 
   context "instance methods" do
     it "defines a create method" do
-      expect(ass).to respond_to(:create)
+      expect(avss).to respond_to(:create)
     end
 
     it "defines an update alias" do
-      expect(ass).to respond_to(:update)
-      expect(ass.method(:update)).to eql(ass.method(:create))
+      expect(avss).to respond_to(:update)
+      expect(avss.method(:update)).to eql(avss.method(:create))
     end
 
     it "defines a delete method" do
-      expect(ass).to respond_to(:delete)
+      expect(avss).to respond_to(:delete)
     end
 
     it "defines a get method" do
-      expect(ass).to respond_to(:get)
+      expect(avss).to respond_to(:get)
     end
 
     it "defines a stop method" do
-      expect(ass).to respond_to(:list)
+      expect(avss).to respond_to(:list)
     end
   end
 end
