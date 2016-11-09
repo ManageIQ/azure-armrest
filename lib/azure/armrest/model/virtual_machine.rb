a336e36b6ba69cb6d55c54243b5782be34966df1
Daniel Berger <dberger@redhat.com>
2016-11-09 15:20:04 -0700

    Alter methods to accept a configuration hash instead of a specific service class instance.

--- lib/azure/armrest/model/virtual_machine.rb
+++ lib/azure/armrest/model/virtual_machine.rb
@@ -8,11 +8,13 @@ module Azure
       # Return the storage account for the current virtual machine model. The
       # argument must be a StorageAccountService object.
       #
-      def storage_account(storage_account_service)
+      def storage_account(config)
         uri = Addressable::URI.parse(properties.storage_profile.os_disk.vhd.uri)
 
         # The uri looks like https://foo123.blob.core.windows.net/vhds/something123.vhd
         name = uri.host.split('.').first # storage name, e.g. 'foo123'
+
+        storage_account_service = Azure::Armrest::StorageAccountService.new(config)
 
         # Look for the storage account in the VM's resource group first. If
         # it's not found, look through all the storage accounts.
@@ -30,14 +32,16 @@ module Azure
       # Get information for the underlying VHD file backing the VM. The
       # argument must be a StorageAccountService object.
       #
-      def virtual_disk(storage_account_service)
+      def virtual_disk(config)
         uri = Addressable::URI.parse(properties.storage_profile.os_disk.vhd.uri)
 
         # The uri looks like https://foo123.blob.core.windows.net/vhds/something123.vhd
         disk = File.basename(uri.to_s)       # disk name, e.g. 'something123.vhd'
         path = File.dirname(uri.path)[1..-1] # container, e.g. 'vhds'
 
-        acct = storage_account(storage_account_service)
+        storage_account_service = Azure::Armrest::StorageAccountService.new(config)
+
+        acct = storage_account(config)
         keys = storage_account_service.list_account_keys(acct.name, acct.resource_group)
         key  = keys['key1'] || keys['key2']
 
