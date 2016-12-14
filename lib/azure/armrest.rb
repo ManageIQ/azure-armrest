43e6da30dca3227786f6e084ca21f3270ba73834
Daniel Berger <dberger@redhat.com>
2016-12-14 10:43:53 -0700

    Fix and update endpoints.

--- lib/azure/armrest.rb
+++ lib/azure/armrest.rb
@@ -18,10 +18,10 @@ module Azure
     USGOV_RESOURCE = "https://management.core.usgovcloudapi.net/"
 
     # The default (public) authority resource
-    AUTHORITY = "https://login.windows.net/"
+    AUTHORITY = "https://login.microsoftonline.com/"
 
     # The authority for US Government clients
-    USGOV_AUTHORITY = "https://login-us.windows.net/"
+    USGOV_AUTHORITY = "https://login-us.microsoftonline.com/"
 
     # Environment string used to indicate US Government
     USGOV_ENVIRONMENT = 'usgov'
