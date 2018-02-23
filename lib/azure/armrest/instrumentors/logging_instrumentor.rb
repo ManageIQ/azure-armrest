555c21d6c977d708066eeb710f62b97139a529d7
Daniel Berger <dberger@redhat.com>
2018-02-23 13:18:21 -0700

    Updated the module scoping.

--- lib/azure/armrest/instrumentors/logging_instrumentor.rb
+++ lib/azure/armrest/instrumentors/logging_instrumentor.rb
@@ -1,55 +1,64 @@
 require 'logger'
 
+# A default logging instrumentor that you can use for development or testing.
+# However, each application should create its own instrumentor in production
+# in order to avoid conflicting with another library or application that
+# uses this library.
+
 module Excon
-  class LoggingInstrumentor
-    def self.logger
-      @logger ||= Logger.new($stderr)
-    end
+  module Azure
+    module Armrest
+      class LoggingInstrumentor
+        def self.logger
+          @logger ||= Logger.new($stderr)
+        end
 
-    def self.logger=(logger)
-      @logger = logger
-    end
+        def self.logger=(logger)
+          @logger = logger
+        end
 
-    def self.instrument(name, params = {}, &block)
-      params = params.dup
+        def self.instrument(name, params = {}, &block)
+          params = params.dup
 
-      # reduce duplication/noise of output
-      params.delete(:connection)
-      params.delete(:stack)
-      params.delete(:ciphers)
-      params.delete(:instrumentor_name)
+          # reduce duplication/noise of output
+          params.delete(:connection)
+          params.delete(:stack)
+          params.delete(:ciphers)
+          params.delete(:instrumentor_name)
 
-      if params.has_key?(:headers) && params[:headers].has_key?('Authorization')
-        params[:headers] = params[:headers].dup
-        params[:headers]['Authorization'] = "REDACTED"
-      end
-
-      if params.has_key?(:password)
-        params[:password] = "REDACTED"
-      end
-
-      if name.include?('request')
-        info = "request: " + params[:scheme] + "://" + File.join(params[:host], params[:path])
-
-        if params[:query]
-          info << "?"
-
-          if params[:query].is_a?(Hash)
-            info << params.to_a.map{ |key,value| "#{key}=#{value}" }.join("&")
-          else
-            info << params[:query]
+          if params.has_key?(:headers) && params[:headers].has_key?('Authorization')
+            params[:headers] = params[:headers].dup
+            params[:headers]['Authorization'] = "REDACTED"
           end
-        end
-      else
-        response_type = name.split('.').last
-        if params[:body]
-          info = "#{response_type}: " + params[:body]
+
+          if params.has_key?(:password)
+            params[:password] = "REDACTED"
+          end
+
+          if name.include?('request')
+            info = "request: " + params[:scheme] + "://" + File.join(params[:host], params[:path])
+
+            if params[:query]
+              info << "?"
+
+              if params[:query].is_a?(Hash)
+                info << params.to_a.map{ |key,value| "#{key}=#{value}" }.join("&")
+              else
+                info << params[:query]
+              end
+            end
+          else
+            response_type = name.split('.').last
+            if params[:body]
+              info = "#{response_type}: " + params[:body]
+            end
+          end
+
+          logger.log(logger.level, info) if info
+
+          yield if block_given?
         end
       end
-
-      logger.log(logger.level, info) if info
-
-      yield if block_given?
     end
   end
 end
