522a0b15fa5eb0661bbcc9a52091a762a0a71551
Adam Grare <adam@grare.com>
2021-08-11 12:49:33 -0400

    Add Microsoft.DBforMySQL services

--- lib/azure/armrest/model/base_model.rb
+++ lib/azure/armrest/model/base_model.rb
@@ -338,6 +338,8 @@ module Azure
     end
 
     module Sql
+      class MysqlServer < BaseModel; end
+      class MysqlDatabase < BaseModel; end
       class PostgresqlServer < BaseModel; end
       class PostgresqlDatabase < BaseModel; end
       class SqlServer < BaseModel; end
